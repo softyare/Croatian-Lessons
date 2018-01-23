9dec5cb43ac3214fa052e0dcc17d412f4767b3d7
Softyare <zarko@considero.net>
2018-01-23 18:13:41 +0100

    Testing Changes

--- CroatianLessons.Standard/CroatianLessons.Standard/Helpers/ILocalize.cs
+++ CroatianLessons.Standard/CroatianLessons.Standard/Helpers/ILocalize.cs
@@ -4,6 +4,10 @@ namespace CroatianLessons.Standard.Helpers
 {
     public interface ILocalize
     {
+        /// <summary>
+        /// Gets device's culture info
+        /// </summary>
+        /// <returns>CultureInfo</returns>
         CultureInfo GetCultureInfo();
         void SetLocale(CultureInfo ci);
     }
