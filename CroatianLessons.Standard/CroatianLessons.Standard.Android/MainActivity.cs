005a9648790ec5a6b306cfbefb422e7c19c30e9c
Softyare <zarko@considero.net>
2018-05-25 05:53:29 +0200

    android ikone i appname localization

--- CroatianLessons.Standard/CroatianLessons.Standard.Android/MainActivity.cs
+++ CroatianLessons.Standard/CroatianLessons.Standard.Android/MainActivity.cs
@@ -9,14 +9,17 @@ using Android.OS;
 
 namespace CroatianLessons.Standard.Droid
 {
-    [Activity(Label = "CroatianLessons", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "@string/app_name", Icon = "@drawable/icon", Theme = "@style/splashscreen", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.Locale)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle bundle)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
-
+            base.Window.RequestFeature(WindowFeatures.ActionBar);
+            // Name of the MainActivity theme you had there before.
+            // Or you can use global::Android.Resource.Style.ThemeHoloLight
+            base.SetTheme(Resource.Style.MainTheme);
             base.OnCreate(bundle);
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
